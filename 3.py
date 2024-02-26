b342e7a64a9a75acf15fa574c63f85064eb3be28
sineokayaa <a.sineokaya@g.nsu.ru>
2024-02-26 23:04:17 +0700

    1

--- 3.py
+++ 3.py
@@ -1,10 +1,13 @@
 n = int(input())
-antnms = {}
-
+antnms_1 = {}
+antnms_2 = {}
 for i in range(n):
     str = input().split()
-    antnms[str[0]] = str[1]
+    antnms_1[str[0]] = str[1]
+    antnms_2[str[1]] = str[0]
 
 word = input()
-
-print(antnms.get(word, word))
+if word in list(antnms_1.keys()):
+    print(antnms_1.get(word, word))
+else:
+    print(antnms_2.get(word, word))
